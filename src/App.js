37017fc8128dd0c6cfc4727b43392050c37f7c9f
Laurent Blondy <laurent.blondy1@gmail.com>
2020-07-08 15:56:32 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import './utils/setDefaultLocalStorage';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GlobalContext from './GlobalContext.js';
 import { GlobalStyles } from './GlobalStyles.js';
-import { LoginPage } from './pages/login';
-// import { ItRoutes } from './pages/it-routes';
-import { CommonRoutes } from './pages/common-routes';
 import Router from './router/Router';
 
 function App() {
